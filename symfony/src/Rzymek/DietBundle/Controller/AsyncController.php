9e68cfe2bd8b6f97439a1c45bbb6d2fcc8df89cb
rzymek01 <rzymek01@gmail.com>
2014-01-15 02:08:31 +0100

    działa aktualizacja profilu

--- symfony/src/Rzymek/DietBundle/Controller/AsyncController.php
+++ symfony/src/Rzymek/DietBundle/Controller/AsyncController.php
@@ -15,7 +15,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AsyncController extends Controller {
     public function profileAction() {
-        $json = "{'success':1,'data':{}}";
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $auth = new Auth($em);
+            $user = $auth->auth();
+
+            $data = $this->get('request')->request->get('data');
+            if (empty($data)) {
+                throw new \RuntimeException();
+            }
+
+            $obj = json_decode($data);
+            $user->setImieNazwisko($obj->imie . ' ' . $obj->nazwisko);
+            $user->setWzrost($obj->wzrost);
+            $user->setWiek($obj->wiek);
+            $user->setWaga($obj->waga);
+            $user->setPlec($obj->plec);
+            $user->setBodyDensityBiodro(('kobieta' == $obj->plec) ?
+                $obj->bodyDensity->biodro : $obj->bodyDensity->pepek);
+            $user->setBodyDensityTriceps(('kobieta' == $obj->plec) ?
+                $obj->bodyDensity->triceps : $obj->bodyDensity->klatka);
+            $user->setBodyDensityUdo($obj->bodyDensity->udo);
+
+            $userRepo = new UzytkownicyRepo($em);
+            $userRepo->update($user);
+
+            $json = "{'success':1,'msg':'OK'}";
+        } catch (\RuntimeException $e) {
+            $json = "{'success':0,'msg':null}";
+        }
+
         return $this->render('DietBundle:Default:async.json.twig', array(
             'json' => $json
         ));
